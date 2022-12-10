074b569fb568e5210d3f3fd2a4bae90158f02053
arun-hel <arun@higheredlab.com>
2022-12-10 15:39:54 +0000

    Added 404 page

---
+++ frontend/src/page/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="page-not-found">
+      <div>
+        <h1>404</h1>
+        <h1>Page Not Found</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
