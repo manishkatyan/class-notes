d45963d6f3ad6affbcfcf56962e40b211725c85a
arun-hel <arun@higheredlab.com>
2022-12-10 15:43:00 +0000

    Added antd

--- frontend/src/page/NotFound.jsx
+++ frontend/src/page/NotFound.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import { Button, Result } from "antd";
 
 const NotFound = () => {
   return (
-    <div className="page-not-found">
-      <div>
-        <h1>404</h1>
-        <h1>Page Not Found</h1>
-      </div>
-    </div>
+    <Result
+      status="403"
+      title="403"
+      subTitle="Sorry, you are not authorized to access this page."
+    />
   );
 };
 
