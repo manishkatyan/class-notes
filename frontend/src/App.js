0f4d3917b221598bab44a56f63611566be9ea038
arun-hel <arun@higheredlab.com>
2022-12-10 15:59:22 +0000

    Added  App layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./page/NotFound";
+import { Layout } from "antd";
 import "./App.css";
+import AppHeader from "./components/Header";
+import AppFooter from "./components/Footer";
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<NotFound />} />
-      </Routes>
+      <Layout>
+        <AppHeader />
+        <Routes>
+          <Route path="/" element={<NotFound />} />
+        </Routes>
+        <AppFooter />
+      </Layout>
     </Router>
   );
 }
